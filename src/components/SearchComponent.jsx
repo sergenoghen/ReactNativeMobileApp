f12476e9845db5d1cd3a6b59f0896c1a3fa51893
Evadave4 <sergenoghen@yahoo.fr>
2023-03-29 09:53:41 -0400

    implementing a simple search page v1.1

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -1,35 +1,51 @@
 import React, { Component } from 'react';
-import { Text, TextInput, View } from 'react-native';
-import  SearchList  from './SearchList';
+import { Button, Text, TextInput, View, Modal } from 'react-native';
+import  List  from './SearchList';
 import { Observable, ObservableInput } from 'rxjs';
+import { Picker } from "@react-native-picker/picker";
 
 export default class SearchComponent extends Component {
   
   constructor(props) {
     super(props);
-    this.state = {text: '', found:'null'};
+    this.state = {text: '', found:['']};
   }
 
   stateChanged(text){
-    const found = SearchList.map((data, key) => {
-      return data.name.includes(text)&&text.length > 0?data.name+'\n':'';
+    const found = 
+    List.CakeType.map((data, key) => {
+      if(text.length > 0 && data.name.startsWith(text)){
+        return data.name;
+      }
     });
     
     this.setState({text, found});
   }
 
+
   render() {
     return (
       <View style={{padding: 10}}>
         <TextInput
-          style={{height: 40, width:'100%', borderColor:'red', borderWidth:1, borderRadius:5}}
+          style={{height: 40, width:'100%', borderColor:'grey', borderWidth:1, borderRadius:5}}
           placeholder="Type here to search in the list!"
           onChangeText={(text) => this.stateChanged(text)}
           value={this.state.text}
         />
-        <Text style={{padding: 10, fontSize: 20}}>
-          {this.state.found.length == 0 || this.state.found == 'null' ?'No results':this.state.found }
-        </Text>
+        <View style={{padding: 10, fontSize: 20}}>
+          {
+            this.state.found.length == 0  ?'No results':this.state.found.map((e) => {
+              if(e) 
+              return (
+                <Button style={{backgroundColor:'yellow', marginTop:30 }}
+                  title={e} onPress={()=>{
+                    alert(e)
+                  }}
+                />
+              )
+            })
+          }
+        </View>
         
       </View>
     );
--- src/components/SearchList.tsx
+++ src/components/SearchList.tsx
@@ -1,5 +1,19 @@
 const  SearchList = [
-    { id: 12, name: 'Dr. Nice' },
+    { 
+      id: 12, 
+      name: 'Dr. Nice', 
+      data:{
+        profile:{
+
+        },
+        location:{
+
+        },
+        professionnal:{
+
+        }
+      } 
+    },
     { id: 13, name: 'Bombasto' },
     { id: 14, name: 'Celeritas' },
     { id: 15, name: 'Magneta' },
@@ -10,4 +24,101 @@ const  SearchList = [
     { id: 20, name: 'Tornado' },
   ];
 
-  export  default SearchList;
+  const CakeType = 
+  [
+    {
+      "id": "0001",
+      "type": "donut",
+      "name": "Cake",
+      "ppu": 0.55,
+      "batters":
+        {
+          "batter":
+            [
+              { "id": "1001", "type": "Regular" },
+              { "id": "1002", "type": "Chocolate" },
+              { "id": "1003", "type": "Blueberry" },
+              { "id": "1004", "type": "Devil's Food" }
+            ]
+        },
+      "topping":
+        [
+          { "id": "5001", "type": "None" },
+          { "id": "5002", "type": "Glazed" },
+          { "id": "5005", "type": "Sugar" },
+          { "id": "5007", "type": "Powdered Sugar" },
+          { "id": "5006", "type": "Chocolate with Sprinkles" },
+          { "id": "5003", "type": "Chocolate" },
+          { "id": "5004", "type": "Maple" }
+        ]
+    },
+    {
+      "id": "0002",
+      "type": "donut",
+      "name": "Raised",
+      "ppu": 0.55,
+      "batters":
+        {
+          "batter":
+            [
+              { "id": "1001", "type": "Regular" }
+            ]
+        },
+      "topping":
+        [
+          { "id": "5001", "type": "None" },
+          { "id": "5002", "type": "Glazed" },
+          { "id": "5005", "type": "Sugar" },
+          { "id": "5003", "type": "Chocolate" },
+          { "id": "5004", "type": "Maple" }
+        ]
+    },
+    {
+      "id": "0003",
+      "type": "donut",
+      "name": "Old Fashioned",
+      "ppu": 0.55,
+      "batters":
+        {
+          "batter":
+            [
+              { "id": "1001", "type": "Regular" },
+              { "id": "1002", "type": "Chocolate" }
+            ]
+        },
+      "topping":
+        [
+          { "id": "5001", "type": "None" },
+          { "id": "5002", "type": "Glazed" },
+          { "id": "5003", "type": "Chocolate" },
+          { "id": "5004", "type": "Maple" }
+        ]
+    },
+    {
+      "id": "0003",
+      "type": "donut",
+      "name": "Old 2 Fashioned",
+      "ppu": 0.55,
+      "batters":
+        {
+          "batter":
+            [
+              { "id": "1001", "type": "Regular" },
+              { "id": "1002", "type": "Chocolate" }
+            ]
+        },
+      "topping":
+        [
+          { "id": "5001", "type": "None" },
+          { "id": "5002", "type": "Glazed" },
+          { "id": "5003", "type": "Chocolate" },
+          { "id": "5004", "type": "Maple" }
+        ]
+    },
+  ]
+  
+  const List={SearchList,CakeType};
+
+
+
+  export  default List;
